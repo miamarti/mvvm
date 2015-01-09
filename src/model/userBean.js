ae6c5f5f64484f48ff9836cd7c004f05f7d594fb
Miller Augusto <miller.augusto@gmail.com>
2015-01-09 00:37:54 -0200

    Create userBean.js

---
+++ src/model/userBean.js
@@ -0,0 +1,8 @@
+cit.model('userBean', function($scope){
+"use strict";
+    var userBean = function(){
+        this.name = 'Maria José';
+        this.fone = '+55 19 9.8177-2939';
+    };
+    return userBean;
+});
